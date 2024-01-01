dd28c083ee97bae82807d44b607bdf68aef9641a
root <root@HP>
2024-01-01 23:34:38 +0100

    route

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -31,5 +31,16 @@ def hbnb(strict_slashes=False):
     return 'HBNB'
 
 
+@app.route('/c/<path:subpath>')
+def cisfun(subpath, strict_slashes=False):
+    """
+    HBNB
+
+    Returns:
+        [String] -- [HBNB]
+    """
+    return 'C {}'.format(subpath.replace('_', ' '))
+
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
